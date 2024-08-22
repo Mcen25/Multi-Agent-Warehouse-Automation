21bac5773b5057354f75dbec1132507426ba91d6
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-08-22 13:09:52 -0400

    Move to goal is finished

--- Warehouse/Assets/Scripts/MoveToGoal.cs
+++ Warehouse/Assets/Scripts/MoveToGoal.cs
@@ -3,16 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
 public class MoveToGoal : Agent
 {
+
+    [SerializeField] private Transform goal;
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(goal.position);
+    }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        base.OnActionReceived(actions);
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        float moveSpeed = 5f;
+
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
     }
 
     public override void OnEpisodeBegin()
     {
-        base.OnEpisodeBegin();
+        transform.localPosition = new Vector3(0, 0.5f, 0);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxis("Horizontal");
+        continuousActions[1] = Input.GetAxis("Vertical");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Goal>(out Goal goal))
+        {
+            SetReward(1f);
+            EndEpisode();
+        }
+
+        if (other.TryGetComponent<Wall>(out Wall wall))
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+        SetReward(1f);
+        EndEpisode();
     }
 }
